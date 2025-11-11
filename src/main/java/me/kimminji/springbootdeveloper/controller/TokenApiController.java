83707e36fd53b1c4863842dee03a70881c51b2c6
hei-sso <lak78998@gmail.com>
2025-11-11 15:36:39 +0900

    Token Controller

---
+++ src/main/java/me/kimminji/springbootdeveloper/controller/TokenApiController.java
@@ -0,0 +1,24 @@
+package me.kimminji.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.kimminji.springbootdeveloper.dto.CreateAccessTokenRequest;
+import me.kimminji.springbootdeveloper.dto.CreateAccessTokenResponse;
+import me.kimminji.springbootdeveloper.service.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
---
+++ src/main/java/me/kimminji/springbootdeveloper/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,10 @@
+package me.kimminji.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+    private String refreshToken;
+}
