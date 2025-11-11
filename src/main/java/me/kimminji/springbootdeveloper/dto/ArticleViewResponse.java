d5fa56a9392da247ebd394d22b3d3cbf399089de
hei-sso <lak78998@gmail.com>
2025-11-11 13:55:27 +0900

    My Blog View

---
+++ src/main/java/me/kimminji/springbootdeveloper/dto/ArticleViewResponse.java
@@ -0,0 +1,23 @@
+package me.kimminji.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.kimminji.springbootdeveloper.domain.Article;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+}
