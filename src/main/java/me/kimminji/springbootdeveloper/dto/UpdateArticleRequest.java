164783dbd32c13b872ffa5cb68c34f44c1a407c0
hei-sso <lak78998@gmail.com>
2025-11-11 13:15:48 +0900

    Upload new files

---
+++ src/main/java/me/kimminji/springbootdeveloper/dto/UpdateArticleRequest.java
@@ -0,0 +1,13 @@
+package me.kimminji.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String content;
+}
